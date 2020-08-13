fe57b09f4abd6a441bb7d34852a6c8cca08b64d8
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-08-13 12:58:15 +0300

    Update install_dotnet_windows7sp1.ps1

--- install_dotnet_windows7sp1.ps1
+++ install_dotnet_windows7sp1.ps1
@@ -79,6 +79,7 @@ If ($PSVersionTable.PSVersion.Major -eq 2) {
     $download_path = "C:\install\Win7-KB3191566-x86.zip" 
     (New-Object Net.WebClient).DownloadFile($download_url, $download_path)
 
+    Expand-ZipFile $download_path -destination c:\install
 
   }
 
