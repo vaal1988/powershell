9dc7daf130b4c06a9d9c1231b3cc9cbde5310761
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-08-13 14:01:57 +0300

    Update update_powershell.ps1

--- update_powershell.ps1
+++ update_powershell.ps1
@@ -1,29 +1,18 @@
 # powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vaal1988/powershell/master/update_powershell.ps1'))"
 
-
+# Throw
 
 $osVersion = (Get-WmiObject Win32_OperatingSystem).version
-
-if($osVersion -eq '6.1.7600') {
-   Throw "SP1 must be installed"
-}
-
 if($osVersion -eq '6.1.7601') {
    Write-Output "Windows version - Win7 SP1/2008R2 SP1"
 }else {
    Throw "Script only for  Win7 SP1/2008R2 SP1"
 }
 
-
-If ((get-service wuauserv).starttype -ieq 'Disabled')
-{
-  Throw "Windows Update Service is disabled - PowerShell updates are distributed as windows updates and so require the service."
-}
-
-
-
-
-
+# If ((get-service wuauserv).starttype -ieq 'Disabled')
+# {
+#   Throw "Windows Update Service is disabled - PowerShell updates are distributed as windows updates and so require the service."
+# }
 
 
 
@@ -37,7 +26,7 @@ $MinimumNet4Version = 378389
 $Net4Version = (get-itemproperty "hklm:software\microsoft\net framework setup\ndp\v4\full" -ea silentlycontinue | Select -Expand Release -ea silentlycontinue)
 
 if ($Net4Version -ge $MinimumNet4Version) {
-  Write-Output "dotNET Framework 4.5.2 or later"
+  Write-Output "dotNET Framework already 4.5.2 or later"
 }
 
 else {
@@ -54,28 +43,30 @@ Start-Process "$download_path" -Wait -ArgumentList "/Quiet /NoRestart"
 
 }
 
-function Expand-ZIPFile($file, $destination)
-{
-$shell = new-object -com shell.application
-$zip = $shell.NameSpace($file)
-foreach($item in $zip.items())
-{
-$shell.Namespace($destination).copyhere($item)
-}
+function Expand-ZIPFile($file, $destination) {
+  $shell = new-object -com shell.application
+  $zip = $shell.NameSpace($file)
+  foreach($item in $zip.items())
+  {
+  $shell.Namespace($destination).copyhere($item)
+  }
 }
 
-
+## updating PS
 
 If ($PSVersionTable.PSVersion.Major -eq 2) {
   Write-Output "PSVersion is 2"
+  Write-Output "Installing"
+
+  if (!(Test-Path C:\install\KB3191566)) { New-Item -ItemType Directory -Force -Path C:\install\KB3191566 }
 
   if ($env:PROCESSOR_ARCHITECTURE -eq "amd64") {
 
     $ps_download_url = "https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7AndW2K8R2-KB3191566-x64.zip" 
     $ps_download_path = "C:\install\Win7AndW2K8R2-KB3191566-x64.zip" 
-    (New-Object Net.WebClient).DownloadFile($download_url, $download_path) 
-
-
+    (New-Object Net.WebClient).DownloadFile($ps_download_url, $ps_download_path)
+    Expand-ZIPFile $ps_download_path -destination C:\install\KB3191566
+    Start-Process "C:\install\KB3191566\Win7AndW2K8R2-KB3191566-x64.msu" -Wait -ArgumentList "/quiet /norestart"
 
   } 
 
@@ -84,34 +75,11 @@ If ($PSVersionTable.PSVersion.Major -eq 2) {
     $ps_download_url = "https://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win7-KB3191566-x86.zip" 
     $ps_download_path = "C:\install\Win7-KB3191566-x86.zip" 
     (New-Object Net.WebClient).DownloadFile($ps_download_url, $ps_download_path)
-
-    Expand-ZIPFile $ps_download_path -destination c:\install
-    
-    Start-Process "C:\install\Win7-KB3191566-x86.msu" -Wait -ArgumentList "/quiet /norestart"
+    Expand-ZIPFile $ps_download_path -destination C:\install\KB3191566
+    Start-Process "C:\install\KB3191566\Win7-KB3191566-x86.msu" -Wait -ArgumentList "/quiet /norestart"
 
   }
 
+  Remove-Item -Path "C:\install\KB3191566" -Recurse
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
