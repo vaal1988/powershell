5f29cc178366c4053e0c89f39242b93f409c412b
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-08-13 11:00:38 +0300

    Update install_dotnet_windows7sp1.ps1

--- install_dotnet_windows7sp1.ps1
+++ install_dotnet_windows7sp1.ps1
@@ -4,20 +4,22 @@ $MinimumNet4Version = 378389
 $Net4Version = (get-itemproperty "hklm:software\microsoft\net framework setup\ndp\v4\full" -ea silentlycontinue | Select -Expand Release -ea silentlycontinue)
 
 if ($Net4Version -ge $MinimumNet4Version) {
-  
   throw ".NET Framework 4.5.2 or later"
 }
 
 else {
 
-# download
 if (!(Test-Path C:\install)) { 
   New-Item -ItemType Directory -Force -Path C:\install 
 }
-$url = "http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe" 
-$path = "C:\install\NDP452-KB2901907-x86-x64-AllOS-ENU.exe" 
 
-(New-Object Net.WebClient).DownloadFile($url, $path) 
+# download
+$download_url = "http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe" 
+$download_path = "C:\install\NDP452-KB2901907-x86-x64-AllOS-ENU.exe" 
+(New-Object Net.WebClient).DownloadFile($download_url, $download_path) 
 
 # install
+Start-Process "$download_path" -Wait -ArgumentList "/Quiet /NoRestart"
+
+
 }
