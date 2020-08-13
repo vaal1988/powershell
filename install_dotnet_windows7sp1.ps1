0cae526a8de9a158a71da117ea436d10907a1df2
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-08-13 11:26:24 +0300

    Update install_dotnet_windows7sp1.ps1

--- install_dotnet_windows7sp1.ps1
+++ install_dotnet_windows7sp1.ps1
@@ -1,5 +1,15 @@
 # powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vaal1988/powershell/master/install_dotnet_windows7sp1.ps1'))"
 
+$osVersion = (Get-WmiObject Win32_OperatingSystem).version
+
+if($osVersion -eq '6.1.7601') {
+   Write-Output "Windows version - Win7 SP1/2008R2 SP1"
+}else {
+   Throw "Script only for  Win7 SP1/2008R2 SP1"
+}
+
+## updating dotNet
+
 $MinimumNet4Version = 378389
 $Net4Version = (get-itemproperty "hklm:software\microsoft\net framework setup\ndp\v4\full" -ea silentlycontinue | Select -Expand Release -ea silentlycontinue)
 
@@ -20,6 +30,5 @@ $download_path = "C:\install\NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
 
 # install
 Start-Process "$download_path" -Wait -ArgumentList "/Quiet /NoRestart"
-
 
 }
