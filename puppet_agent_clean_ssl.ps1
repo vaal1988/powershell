8b34bc915a854ebce0cc5df3c0e519b3a7012461
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-10-19 17:31:39 +0300

    Update puppet_agent_clean_ssl.ps1

--- puppet_agent_clean_ssl.ps1
+++ puppet_agent_clean_ssl.ps1
@@ -1,9 +1,9 @@
 # powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vaal1988/powershell/master/puppet_agent_clean_ssl.ps1'))"
 
-try { 
-  puppet agent -t
-} catch {
-  echo "ssl clean"
+Invoke-Command -ScriptBlock { puppet agent -t } | Tee-Object -Variable PuppetAgentOutput
+
+if ( $PuppetAgentOutput -contains 'Applying configuration') { 
+
+} else {
   puppet ssl clean
-  puppet agent -t
 }
