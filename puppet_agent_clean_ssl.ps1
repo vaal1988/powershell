ed50d0a58c1cf65b1f0ff78eedfa50f5e7f1e6c3
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-10-19 16:10:28 +0300

    Update puppet_agent_clean_ssl.ps1

--- puppet_agent_clean_ssl.ps1
+++ puppet_agent_clean_ssl.ps1
@@ -1,3 +1,5 @@
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vaal1988/powershell/master/install_puppetagent.ps1'))
+
 try { 
   puppet agent -t
 } catch { 
