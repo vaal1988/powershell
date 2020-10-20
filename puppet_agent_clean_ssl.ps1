c894c7a2eb6a4b1eb56ab42100b519512c25d265
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2020-10-20 10:16:33 +0300

    Delete puppet_agent_clean_ssl.ps1

--- puppet_agent_clean_ssl.ps1
+++
@@ -1,9 +0,0 @@
-# powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/vaal1988/powershell/master/puppet_agent_clean_ssl.ps1'))"
-
-Invoke-Command -ScriptBlock { puppet agent -t } | Tee-Object -Variable PuppetAgentOutput
-
-if ( $PuppetAgentOutput -contains 'Caching catalog') { 
-
-} else {
-  puppet ssl clean
-}
