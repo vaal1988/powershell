463f97edd4a8bf09249ffde0c7f5db1a8a8edcad
vaal1988 <45043510+vaal1988@users.noreply.github.com>
2022-10-16 17:52:33 +0300

    Create installSaltMinion.ps1

---
+++ installSaltMinion.ps1
@@ -0,0 +1,51 @@
+# powershell -NoProfile -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create((irm 'https://gitlab.com/vgusachenko/salt-master/-/raw/main/salt/windows_desktops/states/minion/installSaltMinion.ps1'))) 192.168.56.15 windows_desktops"
+
+param (
+   [string]$SALT_MASTER_SERVER = "192.168.56.15",
+   [string]$SALT_AGENT_ENVIRONMENT = "windows_desktops"
+)
+
+if (!(Test-Path C:\install)) { 
+  New-Item -ItemType Directory -Force -Path C:\install 
+}
+
+$saltminion_id = 'Salt Minion'
+$saltminion_version = '3005.1-1'
+$saltminion_download_path = "https://repo.saltproject.io/windows/Salt-Minion-3005.1-1-Py3-AMD64-Setup.exe"
+$saltminion_x86_download_path = "https://repo.saltproject.io/windows/Salt-Minion-3005.1-1-Py3-x86-Setup.exe"
+
+
+if ($env:PROCESSOR_ARCHITECTURE -eq "amd64") {
+
+    write-host "system is 64 bit"
+    # checking if installed
+    $regkeypath= "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$saltminion_id" 
+    $keyvalue = (Get-ItemProperty $regkeypath -ErrorAction SilentlyContinue).DisplayVersion
+
+    If ($keyvalue -eq $saltminion_version) { 
+    Write-Host "saltminion is already installed" 
+    
+    } Else { 
+    Write-Host "not installed" 
+
+    Write-Host "downloading"
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("$saltminion_download_path","C:\install\Salt-Minion-3005.1-1-Py3-AMD64-Setup.exe")
+
+    Write-Host "trying to install"
+    Start-Process "C:\install\Salt-Minion-3005.1-1-Py3-AMD64-Setup.exe" -Wait -ArgumentList "/S /master=$SALT_MASTER_SERVER /start-minion=0"
+    }
+} 
+
+Else {
+
+    write-host "system is not 64"
+
+}
+
+Set-Content -Path "C:\ProgramData\Salt Project\Salt\conf\minion" -Value "
+master: $SALT_MASTER_SERVER
+environment: $SALT_AGENT_ENVIRONMENT
+"
+
+Restart-Service -Name 'salt-minion'
